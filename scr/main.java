25208d371a31e55a42e33f319f62b2518e4c16b5
viweu <153850831+viweu@users.noreply.github.com>
2024-04-29 18:47:00 +0300

    Update main.java

--- scr/main.java
+++ scr/main.java
@@ -26,4 +26,59 @@ public class Main {
         danya.setPartner(sonya);
         danya.addChildFromThisPartner(sonya, denis);
         danya.addChildFromThisPartner(sonya, ari);
+           /*
+        3-rd wife
+         */
+        System.out.println("2-nd");
+        nikita.setPartner(vika);
+        nikita.addChildFromThisPartner(vika, egor);
+        nikita.addChildFromThisPartner(vika, nina);
+
+          /*
+        Try to use the Service class
+         */
+        Service service = new Service();
+        service.addHumanToFamilyTree(2, danya);
+        service.addHumanToFamilyTree(2, vika);
+        service.addHumanToFamilyTree(1, egor);
+        service.addHumanToFamilyTree(1, denis);
+        service.addHumanToFamilyTree(2, sonya);
+        service.addHumanToFamilyTree(1, ari);
+        service.addHumanToFamilyTree(1, nina);
+
+
+        service.addHumanToFamilyTree(3, "Brizhaty Egor Daniilovich",
+                LocalDate.of(1869, new Random().nextInt(1, 13), new Random().nextInt(1, 28)),
+                LocalDate.of(1938, new Random().nextInt(1, 13), new Random().nextInt(1, 28)),
+                Gender.Male);
+        service.addHumanToFamilyTree(3, "Orlova Sofia Alexeevna",
+                LocalDate.of(1872, new Random().nextInt(1, 13), new Random().nextInt(1, 28)),
+                LocalDate.of(1945, new Random().nextInt(1, 13), new Random().nextInt(1, 28)),
+                Gender.Male);
+        service.setParentsForHuman("Khrushev Nikita Sergeevich", service.findByName("Brizhaty Egor Daniilovich"));
+        service.setParentsForHuman("Khrushev Nikita Sergeevich", service.findByName("Orlova Sofia Alexeevna"));
+        String filePathForTree = "src/family_tree/writer/familyTree.out";
+        /*
+         Serialization using ObjectOutputStream class using the service.
+         Created the method for writing an object as byte code
+         */
+        service.initializationFileHandler();
+        service.writeTreeAsByteCode(service.getFamilyTree(), filePathForTree);
+        //Renewing of an object from a byte code file using the class ObjectInputStream
+        FamilyTree treeRestored = service.readTreeFromByteCodeFile(filePathForTree);
+        /*
+         Different displaying of methods of sorting using the service (List of Humans or List of Names of Humans)
+         */
+        ArrayList<Human> sortedByNameFamily = service.getFamilyTree().sortByName();
+
+         System.out.println(service.getFamilyTree().sortByAge());
+        System.out.println(("==".repeat(20)));
+         System.out.println(service.sort());
+
+        HumanIterator iterator = new HumanIterator(service.getFamilyTree());
+
+
+    }
+}
+
 
